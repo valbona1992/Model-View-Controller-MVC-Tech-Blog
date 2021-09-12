b19de34faed3882c73dbff85f4b214474d56ff11
valbona1992 <valbona12@gmail.com>
2021-09-12 15:43:50 -0500

    additional info to homeroute js file

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
-const router = requite('express').Router();
-const sequelize = requite('../config/connection.js');
+const router = require('express').Router();
+const sequelize = require('../config/connection.js');
 const { Post, User, Comment} = require('../models');
 
 // Gets the homepage rendered
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
             },
             {
                 model: Comment, 
-                attributes: ['id', 'comment, post_id', 'user_id', 'date'],
+                attributes: ['id', 'comment', 'post_id', 'user_id', 'date'],
                 include: {
                     model: User,
                     attributes: ['username']
@@ -29,4 +29,65 @@ router.get('/', (req, res) => {
             loggedIn: req.session.loggedIn
         });
     })
-})
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+// Render single post on a page
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'title', 'date', 'post'],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment, 
+                attributes: ['id', 'comment', 'post_id', 'user_id', 'date'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    .then(postData => {
+        if(!postData) {
+            res.status(404).json({message: "No post found"});
+            return;
+        }
+        const post = postData.get({plane: true});
+        res.render('singlepost', {
+            post,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+});
+
+// gets the login page
+router.get('/login', (req, res) => {
+    if(req.session.loggedIn){
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+// gets the signup page
+router.get('signup', (req, res) => {
+    if(req.session.loggedIn){
+        res.redirect('/');
+        return;
+    }
+    res.render('signup');
+});
+
+module.exports = router;
